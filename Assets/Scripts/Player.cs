8a4bdf9093ae842ec0acb3a2117453f9c1454a46
Vadym Vatsenko <vadim.vatsenko@gmail.com>
2024-01-12 20:17:43 +0200

    Add dash animation and dash logic in player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,14 @@ public class Player : MonoBehaviour
     private Animator _animator; // объявляем переменную _animator (контроллер анимации)
     [SerializeField] private float moveSpeed = 5; // ускорение нашего игрока, так как xInput может быть -1 или 1, то это значение очень маленькое, потому нужно его умножать на ускорение
     [SerializeField] private float jumpForce = 5;
+
+    [Header("Dash info")] // шавка в редакторе для рывка
+    [SerializeField] private float dashSpeed; // скорость рывка
+    [SerializeField] private float dashDuration; // продолжительность рывка
+    private float dashTime; // время рывка
+    [SerializeField] private float dashCooldown; //10 - время перерыва между рывками
+    private float dashCooldownTimer; // таймер перерыва между рывками
+
     private float _xInput; // создали публичную переменную, в нее будет записыватся положение Horizontal
     // private bool _isMoving; // переменная которая будет хранить в себе, двигается ли наш персонаж // удаляем эту переменную, так как она нам нужна будет только в методе AnimatorController()
     private int _facingDir = 1; // по умолчанию маш персонаж смотрит в право, потому значение 1, если бы в противоположную смотрел, то было бы -1
@@ -24,6 +32,11 @@ public class Player : MonoBehaviour
         Movement();
         CheckInput();
         CollisionChecks();
+
+        dashTime -= Time.deltaTime; // время рывка будет уменьшатся каждый кадр
+        dashCooldownTimer -= Time.deltaTime; // время перерыва рывка будет уменьшатся каждый кадр, изначально ничему не равен и сразу уходит в минус
+
+
         AnimatorController();
         FlipController();
     }
@@ -40,10 +53,32 @@ public class Player : MonoBehaviour
         {
             Jump();
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift)) // добавили проверку, если нажали LShift 
+        {
+            DashAbility();
+        }
+    }
+
+    private void DashAbility()
+    {
+        if (dashCooldownTimer < 0) //dashCooldownTimer меньше нуля, а он меньше, так как работает на нем таймер в обратную сторону
+        {
+            dashCooldownTimer = dashCooldown; // записалась 10 в таймер и он пошёл убавлятся выше в Update dashCooldownTimer -= Time.deltaTime
+            dashTime = dashDuration; // когда мы наживаем LShift время рывка становится 2ка(из редактора) и запускается таймер, тот, что сверху
+        }
+
     }
     private void Movement()
     {
-        _rb.velocity = new Vector2(_xInput * moveSpeed, _rb.velocity.y); // xInput(1 или -1) * на ускорение, по y равно самому себе
+        if (dashTime > 0)
+        { // если время рывка больше 0, то выполни рывок
+            _rb.velocity = new Vector2(_xInput * dashSpeed, 0); //_xInput - направление(1 или -1) * на dashSpeed - скорость рывка, а по y - 0, что бы не терял высоту при рывке в воздухе
+        }
+        else
+        {
+            _rb.velocity = new Vector2(_xInput * moveSpeed, _rb.velocity.y); // xInput(1 или -1) * на ускорение, по y равно самому себе
+        }
+
     }
     private void Jump()
     {
@@ -56,6 +91,7 @@ public class Player : MonoBehaviour
         _animator.SetFloat("yVelocity", _rb.velocity.y); // передаем в анимацию позицию игрока по y, чтобы запустить нужную анимацию, падения или прыжка
         _animator.SetBool("isMoving", _isMoving); // _animator обращается к Unity аниматору в котором есть переменная isMoving в которую присваиваем значение _isMoving, потому у нас включается анимация
         _animator.SetBool("isGrounded", _isGrounded); // _animator обращается к Unity аниматору в котором есть переменная isGrounded в которую присваиваем значение _isGrounded, потому у нас включается анимация
+        _animator.SetBool("isDashing", dashTime > 0); // рапускаем рывок, если dashTime > 0
     }
     private void Flip()
     {
